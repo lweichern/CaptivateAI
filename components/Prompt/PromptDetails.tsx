33b2ca62bb6d26caa5e52c7ced4098c83ba32ddb
WeiChern Lim <lweichern@gmail.com>
2023-11-28 22:07:12 +0800

    Fix text wrap in prompt detail popup

--- components/Prompt/PromptDetails.tsx
+++ components/Prompt/PromptDetails.tsx
@@ -11,9 +11,12 @@ import {
 } from "@/components/ui/dialog";
 import { PromptObject } from "./PromptTable";
 import { CopyIcon } from "./Icons";
+import { Separator } from "../ui/separator";
 
 function PromptDetails({ prompt }: { prompt: PromptObject }) {
   const [copyIcon, setCopyIcon] = useState(true);
+
+  console.log("prompttt: ", prompt.prompt);
 
   // Copy function
   const copyToClipboard = () => {
@@ -36,19 +39,25 @@ function PromptDetails({ prompt }: { prompt: PromptObject }) {
           <DialogTitle className="text-2xl text-left">
             Prompt Details
           </DialogTitle>
+          <Separator className=" bg-primary" />
+
           <DialogDescription>
             <span className="text-lg">Prompt:</span> <br />
-            {prompt.prompt_question}
+            {prompt.prompt_question} fkssbfouafe foasif Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Dolore totam in iste delectus
+            tenetur ab adipisci nihil cum tempore excepturi vero possimus quae
+            consequuntur at, libero magnam, iure aspernatur asperiores. - fweoif
+            -sdfd fsd
           </DialogDescription>
-          <DialogDescription className=" whitespace-pre break-words">
-            <span className="text-lg flex justify-between">
+          <DialogDescription>
+            <h3 className="text-lg flex justify-between">
               Answer:{" "}
               <div onClick={copyToClipboard}>
                 <CopyIcon copyIcon={copyIcon} />
               </div>
-            </span>{" "}
+            </h3>{" "}
             <br />
-            <p>{prompt.prompt}</p>
+            <p className=" whitespace-pre-wrap">{prompt.prompt}</p>
           </DialogDescription>
         </DialogHeader>
       </DialogContent>
